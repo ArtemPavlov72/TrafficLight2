6947eba14ce53be49eeb3baa0383e69da090d616
Artem Pavlov <pavlov.tema@gmail.com>
2021-10-26 23:01:01 +0500

    Initial Commit

---
+++ TrafficLight2/TrafficLight2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight2App.swift
+//  TrafficLight2
+//
+//  Created by iMac on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

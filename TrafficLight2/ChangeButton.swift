8a73ee85650c49dfae1e71656b2734737962be5e
Artem Pavlov <pavlov.tema@gmail.com>
2021-10-30 11:47:39 +0500

    ChangeButton added.

---
+++ TrafficLight2/ChangeButton.swift
@@ -0,0 +1,21 @@
+//
+//  ChangeButton.swift
+//  TrafficLight2
+//
+//  Created by Artem Pavlov on 30.10.2021.
+//
+// Выносим кнопку в отдельный View
+
+import SwiftUI
+
+struct ChangeButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChangeButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ChangeButton()
+    }
+}
